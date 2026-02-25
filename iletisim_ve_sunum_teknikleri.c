ce77115ea149eddca727067ab76daecbb723a591
25020091006YarenCirkin <25020091006@ogr.istiklal.edu.tr>
2026-02-26 00:02:09 +0300

    Add files via upload

---
+++ iletisim_ve_sunum_teknikleri.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    printf("İletişim ve Sunum Teknikleri Dersine Hoşgeldiniz!");
+
+    return 0;
+}
